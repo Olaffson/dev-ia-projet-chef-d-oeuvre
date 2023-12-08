8e0ba5da345dbb09ead83c63e79a71bb31e37d91
Olaffson <olivierkotwica@gmail.com>
2023-12-08 10:13:36 +0100

    add patch workflows

--- projet_chef_d_oeuvre/application_chef_d_oeuvre/tests.py
+++ projet_chef_d_oeuvre/application_chef_d_oeuvre/tests.py
@@ -6,12 +6,6 @@ from django.test import Client, TestCase
 # Create your tests here.
 
 
-# @pytest.fixture
-# @pytest.mark.django_db  # Activer l'accès à la base de données pour cette fixture
-# def user():
-#     return User.objects.create_user(username='testuser', password='12345')
-
-
 @pytest.fixture
 @pytest.mark.django_db
 def user():
@@ -52,3 +46,62 @@ def test_index_view_with_authenticated_user(user):
     # Vérifiez si le prénom de l'utilisateur est affiché
     assert response.status_code == 200
     assert 'Bonjour, Test !' in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_logout_user(user):
+    client = Client()
+    client.login(username='testuser', password='12345')
+
+    url = reverse('logout')  # Assurez-vous que 'logout' est le nom de l'URL de votre vue de déconnexion
+    response = client.get(url)
+
+    # Vérifiez si l'utilisateur est déconnecté et redirigé
+    assert response.status_code == 302  # Redirection après déconnexion
+    assert '_auth_user_id' not in client.session  # L'ID utilisateur ne doit pas être dans la session
+
+
+@pytest.mark.django_db
+def test_failed_login():
+    client = Client()
+    url = reverse('login')
+    response = client.post(url, {'username': 'wronguser', 'password': 'wrongpass'})
+
+    # Vérifiez si la connexion a échoué
+    assert response.status_code == 200  # Pas de redirection, reste sur la page de connexion
+    assert 'Identifiants invalides.' in response.content.decode()
+    assert '_auth_user_id' not in client.session
+
+
+@pytest.mark.django_db
+def test_signup_page():
+    client = Client()
+    url = reverse('signup')
+    user_data = {
+        'username': 'newuser',
+        'password1': 'newpass123',
+        'password2': 'newpass123',
+        'first_name': 'New',
+        'last_name': 'User',
+        'email': 'newuser@example.com'
+    }
+    response = client.post(url, user_data)
+
+    # Vérifiez si le nouvel utilisateur est créé et redirigé
+    assert response.status_code == 302  # Redirection après une inscription réussie
+    assert User.objects.filter(username='newuser').exists()  # L'utilisateur a été créé
+
+
+@pytest.mark.django_db
+def test_signup_page_failure():
+    client = Client()
+    url = reverse('signup')
+    response = client.post(url, {'username': 'testuser', 'password1': 'short', 'password2': 'short'})
+
+    # Assurez-vous que la réponse est celle attendue
+    assert response.status_code == 200
+
+    # Vérifiez si le formulaire retourné contient des erreurs
+    assert 'form' in response.context
+    form = response.context['form']
+    assert form.errors  # Vérifiez que le formulaire contient des erreurs
--- projet_chef_d_oeuvre/application_chef_d_oeuvre/views.py
+++ projet_chef_d_oeuvre/application_chef_d_oeuvre/views.py
@@ -6,50 +6,6 @@ from . import forms
 # Create your views here.
 
 
-# def index(request):
-#     return render(request, 
-#                   'application_chef_d_oeuvre/index.html',
-#                   )
-    
-# def logout_user(request):
-    
-#     logout(request)
-#     return redirect('login')
-
-
-
-# def login_page(request):
-#     form = forms.LoginForm()
-#     message = ''
-#     if request.method == 'POST':
-#         form = forms.LoginForm(request.POST)
-#         if form.is_valid():
-#             user = authenticate(
-#                 username=form.cleaned_data['username'],
-#                 password=form.cleaned_data['password'],
-#             )
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect('home')
-#         message = 'Identifiants invalides.'
-#     return render(request, 'application_chef_d_oeuvre/login.html', context={'form': form, 'message': message})
-
-
-# def signup_page(request):
-#     form = forms.SignupForm()
-#     if request.method == 'POST':
-#         form = forms.SignupForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             # auto-login user
-#             login(request, user)
-#             return redirect(settings.LOGIN_REDIRECT_URL)
-#     return render(request, 'application_chef_d_oeuvre/signup.html', context={'form': form})
-
-
-
-
-
 def index(request):
     if request.user.is_authenticated:
         return render(request, 'application_chef_d_oeuvre/index.html', {'first_name': request.user.first_name})
